4a0cf33f720c5a665aeeab1086ec1e33f296c849
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2017-09-16 17:35:38 +0900

    単体テストドライバを作成

---
+++ HouseholdAccountsCore/Lib/Tests/TestDriver.cpp
@@ -0,0 +1,94 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2017, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Driver.
+**
+**      @file       Tests/TestDriver.cpp
+**/
+
+#include    "TestDriver.h"
+
+#if ( HAVE_CPPUNIT )
+#    include    <cppunit/BriefTestProgressListener.h>
+#    include    <cppunit/CompilerOutputter.h>
+#    include    <cppunit/extensions/TestFactoryRegistry.h>
+#    include    <cppunit/TestResult.h>
+#    include    <cppunit/TestResultCollector.h>
+#    include    <cppunit/TestRunner.h>
+#endif
+
+#if ( HAVE_CPPUNIT )
+
+int
+executeCppUnitTests(
+        int     argc,
+        char *  argv[])
+{
+    //  未使用の引数。  //
+    (void)(argc);
+    (void)(argv);
+
+    CPPUNIT_NS::TestResult                  controller;
+    CPPUNIT_NS::TestResultCollector         result;
+
+    CPPUNIT_NS::BriefTestProgressListener   progress;
+    CPPUNIT_NS::TestRunner                  runner;
+
+    //  単体テストの準備。  //
+    controller.addListener( &result );
+    controller.addListener( &progress );
+
+    //  テストを実行する。  //
+    runner.addTest(
+            CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+    runner.run( controller );
+
+    //  テスト結果を表示。  //
+    CPPUNIT_NS::CompilerOutputter
+            outputter( &result, CPPUNIT_NS::stdCOut() );
+    outputter.write();
+
+    return ( result.wasSuccessful() ? 0 : 1 );
+}
+
+#else   //  if ! ( HAVE_CPPUNIT )
+
+int
+executeCppUnitTests(
+        int     argc,
+        char *  argv[])
+{
+    //  未使用の引数。  //
+    (void)(argc);
+    (void)(argv);
+
+    return ( 0 );
+}
+
+//----------------------------------------------------------------
+
+TestFixture::TestFixture()
+{
+}
+
+TestFixture::~TestFixture()
+{
+}
+
+void  TestFixture::setUp()
+{
+}
+
+void  TestFixture::tearDown()
+{
+}
+
+#endif  //  ! ( HAVE_CPPUNIT )
