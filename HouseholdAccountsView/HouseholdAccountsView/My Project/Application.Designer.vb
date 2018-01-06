92b4914ca864e44fd05bf3a3ce92c54dd5fbdc60
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-01-06 19:06:43 +0900

    メインフォームのクラス名を変更

--- HouseholdAccountsView/HouseholdAccountsView/My Project/Application.Designer.vb
+++ HouseholdAccountsView/HouseholdAccountsView/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HouseholdAccountsView.Form1
+            Me.MainForm = Global.HouseholdAccountsView.MainView
         End Sub
     End Class
 End Namespace
