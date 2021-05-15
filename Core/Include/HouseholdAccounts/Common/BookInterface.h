d2e5b818fc56c186ecd770beed73de83b1fbb7b7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-05-15 19:18:21 +0900

    ヘッダファイルを追加

---
+++ Core/Include/HouseholdAccounts/Common/BookInterface.h
@@ -0,0 +1,38 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2021, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/BookInterface.h
+**/
+
+#if !defined( HACCOUNTS_COMMON_INCLUDED_BOOK_INTERFACE_H )
+#    define   HACCOUNTS_COMMON_INCLUDED_BOOK_INTERFACE_H
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
