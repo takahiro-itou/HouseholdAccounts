92f8c33a966923e9cc12eeb33a770071e1d6d105
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-04-18 23:46:46 +0900

    ヘッダファイルを追加

---
+++ View/Wrapper/Common/AccountsTypes.h
@@ -0,0 +1,36 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/AccountsTypes.h
+**/
+
+#pragma     once
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+namespace  AccountsWrapper  {
+
+public  enum class  ErrCode
+{
+    ERR_SUCCESS             = HouseholdAccounts::ERR_SUCCESS,
+    ERR_FAILURE             = HouseholdAccounts::ERR_FAILURE,
+    ERR_FILE_OPEN_ERROR     = HouseholdAccounts::ERR_FILE_OPEN_ERROR,
+    ERR_FILE_IO_ERROR       = HouseholdAccounts::ERR_FILE_IO_ERROR,
+    ERR_INDEX_OUT_OF_RANGE  = HouseholdAccounts::ERR_INDEX_OUT_OF_RANGE
+};
+
+}   //  End of namespace  AccountsWrapper
