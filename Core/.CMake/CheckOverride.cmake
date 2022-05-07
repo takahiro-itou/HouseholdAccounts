aba491a6666d7aeba17152dbf4f3af6571656962
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-05-07 23:49:36 +0900

    リファクタリング

--- Core/.CMake/CheckOverride.cmake
+++ Core/.CMake/CheckOverride.cmake
@@ -1,7 +1,7 @@
 
 Include (CheckCXXSourceCompiles)
 
-Check_CXX_Source_Compiles (
+check_cxx_source_compiles (
    "class A { virtual void f() { } };
     class B : public A { virtual void f() override { } };
     int main () {
@@ -10,8 +10,8 @@ Check_CXX_Source_Compiles (
     CMAKE_CHECK_CXX_OVERRIDE_ENABLED
 )
 If ( CMAKE_CHECK_CXX_OVERRIDE_ENABLED )
-    Set (CONFIG_CHECK_CXX_OVERRIDE_ENABLED  1)
+    set(CONFIG_CHECK_CXX_OVERRIDE_ENABLED   1)
 Else  ()
-    Set (CONFIG_CHECK_CXX_OVERRIDE_ENABLED  0)
+    set(CONFIG_CHECK_CXX_OVERRIDE_ENABLED   0)
 EndIf ()
 
