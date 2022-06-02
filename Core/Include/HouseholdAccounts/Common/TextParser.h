21d0ef15ae87b0e53e97fad54775dd7570292937
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-06-03 01:15:29 +0900

    クラスを新規作成

---
+++ Core/Include/HouseholdAccounts/Common/TextParser.h
@@ -0,0 +1,121 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of TextParser class.
+**
+**      @file       Common/TextParser.h
+**/
+
+#if !defined( HACCOUNTS_COMMON_INCLUDED_TEXT_PARSER_H )
+#    define   HACCOUNTS_COMMON_INCLUDED_TEXT_PARSER_H
+
+#include    "HouseholdAccounts/Common/HouseholdAccountsSettings.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+//  クラスの前方宣言。  //
+
+
+//========================================================================
+//
+//    TextParser  class.
+//
+/**
+**
+**/
+
+class  TextParser
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+public:
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    TextParser();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~TextParser();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   TextParserTest;
+};
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/Common/TextParser.cpp
@@ -0,0 +1,117 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of TextParser class.
+**
+**      @file       Common/TextParser.cpp
+**/
+
+#include    "HouseholdAccounts/Common/TextParser.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+namespace  {
+}   //  End of (Unnamed) namespace.
+
+//========================================================================
+//
+//    TextParser  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+TextParser::TextParser()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+TextParser::~TextParser()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//----------------------------------------------------------------
+//    データをテキストストリームから読み込む。
+//
+
+ErrCode
+BookFile::readFromTextStream(
+        std::istream            &inStr,
+        Documents::BookDocument *ptrDoc)
+{
+    return ( ERR_FAILURE );
+}
+
+//----------------------------------------------------------------
+//    データをテキストストリームに書き込む。
+//
+
+ErrCode
+BookFile::saveToTextStream(
+        const   Documents::BookDocument &objDoc,
+        std::ostream                    &outStr)
+{
+    return ( ERR_FAILURE );
+}
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  FileFormat
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
