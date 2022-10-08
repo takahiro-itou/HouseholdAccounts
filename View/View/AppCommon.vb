5681d9237b1e434c1085ec97fcff28c184076d69
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-08 18:22:17 +0900

    フォームの位置を保存復元する処理を実装

---
+++ View/View/AppCommon.vb
@@ -0,0 +1,78 @@
+Module AppCommon
+
+''------------------------------------------------------------------------
+''    ウィンドウを初期位置に移動する。
+''------------------------------------------------------------------------
+Public Sub moveWindowToStartPosition(
+        ByVal iniFileName As String,
+        ByVal iniSecName As String,
+        ByRef targetForm As System.Windows.Forms.Form,
+        ByRef ownerForm As System.Windows.Forms.Form)
+
+Dim fx As Integer, fy As Integer, fw As Integer, fh As Integer
+Dim bx As Integer, by As Integer, bw As Integer, bh As Integer
+Dim sc As System.Windows.Forms.Screen
+Dim sx As Integer, sy As Integer, sw As Integer, sh As Integer
+Dim sr As Integer, sb As Integer
+
+    With targetForm
+        fw = .Width
+        fh = .Height
+    End With
+
+    sc = System.Windows.Forms.Screen.FromControl(targetForm)
+    With sc.Bounds
+        sx = .Left
+        sy = .Top
+        sw = .Width
+        sh = .Height
+        sr = .Right
+        sb = .Bottom
+    End With
+    If (ownerForm Is Nothing) Then
+        bx = sx
+        by = sy
+        bw = sw
+        bh = sh
+    Else
+        With ownerForm
+            bx = .Left
+            by = .Top
+            bw = .Width
+            bh = .Height
+        End With
+    End If
+
+    fx = getSettingINI(iniFileName, iniSecName, "Left", -1)
+    fy = getSettingINI(iniFileName, iniSecName, "Top", -1)
+    If (fx < sx) Or (fx + fw > sr) Then
+        ' ウィンドウが画面からはみ出す場合は、オーナー中央に移動させる。 '
+        fx = bx + (bw - fw) \ 2
+    End If
+    If (fy < sy) Or (fy + fh > sb) Then
+        ' ウィンドウが画面からはみ出す場合は、オーナー中央に移動させる。 '
+        fy = by + (bh - fh) \ 2
+    End If
+
+    targetForm.Bounds = New Rectangle(fx, fy, fw, fh)
+
+End Sub
+
+''------------------------------------------------------------------------
+''    ウィンドウの現在位置を保存する。
+''------------------------------------------------------------------------
+Public Sub saveWindowPrefs(
+        ByVal iniFileName As String,
+        ByVal iniSecName As String,
+        ByRef sourceForm As System.Windows.Forms.Form)
+
+    With sourceForm
+        saveSettingINI(iniFileName, iniSecName, "Left", .Left)
+        saveSettingINI(iniFileName, iniSecName, "Height", .Height)
+        saveSettingINI(iniFileName, iniSecName, "Top", .Top)
+        saveSettingINI(iniFileName, iniSecName, "Width", .Width)
+    End With
+
+End Sub
+
+End Module
---
+++ View/View/WinAPI.vb
@@ -0,0 +1,74 @@
+''============================================================================
+''
+''============================================================================
+
+Imports System.Runtime.InteropServices
+
+Module WinAPI
+
+''  設定をファイルから読み込む。
+<DllImport("KERNEL32.DLL", CharSet:=CharSet.Auto)>
+Public Function GetPrivateProfileString(
+            ByVal lpAppName As String,
+            ByVal lpKeyName As String,
+            ByVal lpDefault As String,
+            ByVal lpReturnedString As System.Text.StringBuilder,
+            ByVal nSize As Integer,
+            ByVal lpFileName As String) As Integer
+End Function
+
+''  設定をファイルに保存する。
+<DllImport("KERNEL32.DLL")>
+Public Function WritePrivateProfileString(
+            ByVal lpAppName As String,
+            ByVal lpKeyName As String,
+            ByVal lpString As String,
+            ByVal lpFileName As String) As Integer
+End Function
+
+''------------------------------------------------------------------------
+''    アプリケーションの実行ディレクトリを取得する。
+''------------------------------------------------------------------------
+Public Function getAppPath() As String
+
+    getAppPath = System.IO.Path.GetDirectoryName(
+        System.Reflection.Assembly.GetExecutingAssembly().Location)
+
+End Function
+
+''------------------------------------------------------------------------
+''    初期化ファイル（.INIファイル）から設定を読み込む。
+''------------------------------------------------------------------------
+Public Function getSettingINI(
+            ByVal fileName As String,
+            ByVal strSection As String,
+            ByVal strKey As String,
+            ByVal strDefault As String) As String
+
+Dim retVal As Integer
+Dim strBuf As System.Text.StringBuilder
+
+    strBuf = New System.Text.StringBuilder
+    retVal = GetPrivateProfileString(
+                strSection, strKey, strDefault, strBuf,
+                strBuf.Capacity, fileName)
+    getSettingINI = strBuf.ToString()
+
+End Function
+
+''------------------------------------------------------------------------
+''    初期化ファイル（.INIファイル）に設定を書き込む。
+''------------------------------------------------------------------------
+Public Sub saveSettingINI(
+            ByVal fileName As String,
+            ByVal strSection As String,
+            ByVal strKey As String,
+            ByVal strData As String)
+
+Dim retVal As Integer
+
+    retVal = WritePrivateProfileString(strSection, strKey, strData, fileName)
+
+End Sub
+
+End Module
