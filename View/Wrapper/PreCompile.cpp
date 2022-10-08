85f8de3c974ad583ea73a0b84ecf6780399c155a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-08 13:00:45 +0900

    リファクタリング

--- View/Wrapper/PreCompile.cpp
+++ View/Wrapper/PreCompile.cpp
@@ -1,5 +1,20 @@
-// stdafx.cpp : source file that includes just the standard includes
-// HouseholdAccountsWrapper.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+//  PreCompile.cpp: プリコンパイル済みヘッダーに対応するソースファイル
 
 #include    "PreCompile.h"
+
+//  プリコンパイル済みヘッダーを使用している場合、
+//  コンパイルを成功させるにはこのソースファイルが必要です。
