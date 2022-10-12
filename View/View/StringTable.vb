002ae6d172532594924c46de44551a1f1efc1dd3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-12 22:49:13 +0900

    構造体宣言をコピー

--- View/View/StringTable.vb
+++ View/View/StringTable.vb
@@ -2,4 +2,32 @@ Option Explicit
 
 Module StringTable
 
+'*****************************************************************************
+' AccountBook
+' StringTable モジュール
+'
+' 家計簿内の文字列テーブルを管理する
+'
+' Copyright (c) Itou Takahiro, All rights reserved.
+' This file is written in 2006/09/23 - 2008/01/06
+'*****************************************************************************
+
+'*****************************************************************************
+'
+'文字列テーブル
+'
+
+'ソート状態
+Public Const STRINGSORTNONE As Long = 0         'ソートなし
+Public Const STRINGSORTASCENDING As Long = 1    '昇順
+
+Public Structure tStringTable
+    Public nTableBufferSize As Long            'テーブル用のバッファサイズ
+    Public nNumEntry As Long                   '実際に格納されているデータ数
+    Public nSorted As Long                     'ソート状態
+    Public nEntryFlags() As Long               '各エントリのフラグ
+    Public sTableEntries() As String           '文字列テーブル
+    Public nSortIndex() As Long                '昇順にソートしたときの、インデックステーブル
+End Structure
+
 End Module
