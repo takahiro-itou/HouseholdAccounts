8dd1bc3869c252ad77859bdd0ab465f001437d42
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-12 22:51:54 +0900

    関数をコピー

--- View/View/StringTable.vb
+++ View/View/StringTable.vb
@@ -1,4 +1,4 @@
-Option Explicit
+﻿Option Explicit
 
 Module StringTable
 
@@ -29,5 +29,66 @@ Public Structure tStringTable
     Public sTableEntries() As String           '文字列テーブル
     Public nSortIndex() As Long                '昇順にソートしたときの、インデックステーブル
 End Structure
+
+Public Function FindString(ByRef utStringTable As tStringTable, _
+    ByVal strText As String) As Long
+'---------------------------------------------------------------------
+'[ IN] utStringTable: 文字列テーブル
+'[ IN] strText      : 検索する文字列
+'[RET] Long
+'  検索結果
+'[ACT]
+'  指定された文字列テーブルから、
+'strText で指定された文字列を検索し、そのインデックスを返す。
+'  見つからなければ、-1を返す。
+'---------------------------------------------------------------------
+Dim lngIndex As Long, lngResult As Long
+Dim lngLeft As Long, lngRight As Long, lngTarget As Long
+Dim strCheck As String
+
+    With utStringTable
+        lngLeft = 0
+        lngRight = .nNumEntry - 1
+        lngResult = -1
+
+        Do While (lngRight - lngLeft >= 8)
+            lngTarget = (lngLeft + lngRight) \ 2
+            lngIndex = .nSortIndex(lngTarget)
+            strCheck = .sTableEntries(lngIndex)
+
+            If (strCheck = strText) Then
+                '見つかった
+                lngResult = lngIndex
+                lngLeft = lngIndex
+                lngRight = lngIndex
+                Exit Do
+            End If
+
+            '検索範囲を絞る
+            If (strCheck < strText) Then
+                '検索しているデータは、現在位置lngTarget より右にある
+                lngLeft = lngTarget + 1
+            Else
+                '検索しているデータは、現在位置lngTarget より左にある
+                lngRight = lngTarget - 1
+            End If
+        Loop
+
+        'ある程度範囲が小さくなったところで、単純検索に切り替える
+        For lngTarget = lngLeft To lngRight
+            lngIndex = .nSortIndex(lngTarget)
+            strCheck = .sTableEntries(lngIndex)
+
+            If (strCheck = strText) Then
+                '見つかった
+                lngResult = lngIndex
+                Exit For
+            End If
+        Next lngTarget
+    End With
+
+    '結果を返す
+    FindString = lngResult
+End Function
 
 End Module
