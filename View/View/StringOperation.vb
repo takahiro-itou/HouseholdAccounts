92d44f44bc921cfde9cbfab82d271d3ece3f5799
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-17 23:44:05 +0900

    関数をコピー

--- View/View/StringOperation.vb
+++ View/View/StringOperation.vb
@@ -2,4 +2,56 @@ Option Explicit
 
 Module StringOperation
 
+'*****************************************************************************
+' Libary
+' Stringモジュール
+'
+' 文字列を操作する関数群
+'
+' Copyright (c) Itou Takahiro, All rights reserved.
+' This file is written in 2004/08/18 - 2007/12/31
+'*****************************************************************************
+
+Public Function ByteToString(ByRef lpBuffer() As Byte, _
+    ByVal lngStart As Long, ByVal lngEnd As Long, _
+    Optional ByVal blnNullTerm As Boolean = True) As String
+'---------------------------------------------------------------------
+'バイト列を文字列に変換する
+'[ IN] bytBuffer(): バイト列
+'[ IN] lngStart   : 変換を開始する位置(配列のインデックス)
+'[ IN] lngEnd     : 変換を終了する位置(配列のインデックス)
+'[ IN] blnNullTerm: TrueならNull文字を終端とする
+'[RET] String     : 変換結果
+'[ACT]
+'  lngStartからlngEnd までの範囲を
+'(システムで使用している文字コードで)文字列に変換する。
+'---------------------------------------------------------------------
+Dim i As Long
+Dim lpTemp() As Byte
+Dim strText As String
+
+    '指定された範囲を文字列に変換する
+    If lngStart = 0 Then
+        lpTemp() = lpBuffer()
+        ReDim Preserve lpTemp(0 To lngEnd)
+    Else
+        ReDim lpTemp(lngStart To lngEnd)
+        For i = lngStart To lngEnd
+            lpTemp(i) = lpBuffer(i)
+        Next i
+    End If
+    strText = StrConv(lpTemp(), vbUnicode)
+
+    'NULL終端文字で文字列を切る
+    If blnNullTerm Then
+        i = InStr(strText, vbNullChar)
+        If (i > 0) Then
+            strText = Left$(strText, i - 1)
+        End If
+    End If
+
+    '変換結果を返す
+    ByteToString = strText
+End Function
+
 End Module
