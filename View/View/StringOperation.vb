9768a25536e5297b96093ac21a72b857621721e5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-18 20:39:16 +0900

    コンパイルエラーを修正
    (1) 利用できなくなった関数を書き換え

--- View/View/StringOperation.vb
+++ View/View/StringOperation.vb
@@ -40,7 +40,7 @@ Dim strText As String
             lpTemp(i) = lpBuffer(i)
         Next i
     End If
-    strText = StrConv(lpTemp(), vbUnicode)
+    strText = System.Text.Encoding.GetEncoding("utf8").GetString(lpTemp)
 
     'NULL終端文字で文字列を切る
     If blnNullTerm Then
