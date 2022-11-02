53b142115297654cae4b8d5dba5363b0e9d5d0d0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-02 22:49:35 +0900

    ファイルを新規作成

---
+++ View/View/BookItem.vb
@@ -0,0 +1,15 @@
+Option Explicit
+
+Module Declarations
+
+'*****************************************************************************
+' AccountBook
+' BookItem  モジュール
+'
+' 家計簿のデータ項目を管理する
+'
+' Copyright (c) Itou Takahiro, All rights reserved.
+' This file is written in 2006/09/23 - 2008/01/06
+'*****************************************************************************
+
+End Module
