540b586e886cd8df11cd6cdbd976da62dae59dec
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-02 22:54:11 +0900

    サブプロシージャをコピー

--- View/View/UserInterface.vb
+++ View/View/UserInterface.vb
@@ -131,4 +131,89 @@ Dim lngNowShowingRows As Long
     End With
 End Sub
 
+'=========================================================================
+'
+'プライベートプロシージャ
+'
+
+Private Sub UserInterfaceDrawCell(ByRef utUI As tUserInterface, _
+    ByVal lngCol As Long, ByVal lngRow As Long, _
+    ByVal lngLeftMargin As Long, ByVal lngRightMargin As Long, _
+    ByVal strText As String, _
+    ByVal lngArrangeCol As Long, ByVal lngIcon As Long, _
+    Optional ByVal lngMultiCols As Long = 1, Optional ByVal lngMultiRows As Long = 1, _
+    Optional ByVal lngBGColor As OLE_COLOR = &HFFFFFF, _
+    Optional ByVal lngBorderColor As OLE_COLOR = &H0&, _
+    Optional ByVal lngTextColor As OLE_COLOR = &H0&)
+'---------------------------------------------------------------------
+'一つのセルを描画する
+'[ IN] utUI          : ユーザーインターフェイスデータ
+'[ IN] lngCol, lngRow: セルの位置
+'[ IN] lngLeftMargin :
+'[ IN] lngTopMargin  :
+'[ IN] strText       : セルに表示する文字列
+'[ IN] lngArrangeCol :
+'[ IN] lngIcon       : 表示するアイコンの番号、なければ-1
+'[ IN] lngMultiCols  :
+'[ IN] lngMultiRows  :
+'[ IN] lngBGColor    : セルの背景の色
+'[ IN] lngBorderColor: セルの枠の色
+'[ IN] lngTextColor  : テキストの色
+'[RET] なし
+'---------------------------------------------------------------------
+Dim lngDestX As Long, lngDestY As Long, lngSrcX As Long
+Dim lngWidth As Long, lngHeight As Long
+Dim lngTextAreaLeft As Long, lngTextAreaWidth As Long
+Dim lngTextWidth As Long
+Dim hCellPictureDC As Long, lngResult As Long
+
+    If (lngMultiCols <= 0) Then lngMultiCols = 1
+    If (lngMultiRows <= 0) Then lngMultiRows = 1
+
+    With utUI
+        lngWidth = .nCellWidth * lngMultiCols
+        lngHeight = .nCellHeight * lngMultiRows
+        If (lngIcon < 0) Then
+            lngTextAreaLeft = lngLeftMargin + .nCharWidth
+            lngTextAreaWidth = lngWidth - (lngLeftMargin + lngRightMargin) - .nCharWidth * 2
+        Else
+            lngTextAreaLeft = lngLeftMargin + .nCharWidth + 16
+            lngTextAreaWidth = lngWidth - (lngLeftMargin + lngRightMargin + 16) - .nCharWidth * 2
+        End If
+
+        .oCellPicture.Cls
+        .oCellPicture.Line (0, 0)-(lngWidth, lngHeight), lngBGColor, BF
+        .oCellPicture.Line (0, 0)-(lngWidth, lngHeight), lngBorderColor, B
+        hCellPictureDC = .oCellPicture.hDC
+
+        'アイコンを表示する
+        If (lngIcon >= 0) Then
+            lngSrcX = lngIcon * 16
+            lngDestY = (lngHeight - 16) \ 2
+
+            lngResult = BitBlt(hCellPictureDC, lngLeftMargin, lngDestY, 16, 16, .oIconsPicture.hDC, lngSrcX, 16, SRCAND)
+            lngResult = BitBlt(hCellPictureDC, lngLeftMargin, lngDestY, 16, 16, .oIconsPicture.hDC, lngSrcX, 0, SRCPAINT)
+        End If
+
+        '文字を表示する
+        With .oCellPicture
+            lngTextWidth = .TextWidth(strText)
+            If (lngArrangeCol = ACLEFT) Then
+                .CurrentX = lngTextAreaLeft
+            ElseIf (lngArrangeCol = ACRIGHT) Then
+                .CurrentX = lngTextAreaLeft + (lngTextAreaWidth - lngTextWidth)
+            Else
+                .CurrentX = lngTextAreaLeft + (lngTextAreaWidth - lngTextWidth) \ 2
+            End If
+            .CurrentY = (lngHeight - utUI.nCharHeight) \ 2
+            .ForeColor = lngTextColor
+        End With
+        .oCellPicture.Print strText
+
+        lngDestX = lngCol * utUI.nCellWidth
+        lngDestY = lngRow * utUI.nCellHeight
+        lngResult = BitBlt(.oCanvasPicture.hDC, lngDestX, lngDestY, lngWidth + 1, lngHeight + 1, hCellPictureDC, 0, 0, SRCCOPY)
+    End With
+End Sub
+
 End Module
