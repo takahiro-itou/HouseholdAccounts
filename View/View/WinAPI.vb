9de59b963c082194807ceb822a7cb70acb3f8dea
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-03 09:56:13 +0900

    API 宣言を追加

--- View/View/WinAPI.vb
+++ View/View/WinAPI.vb
@@ -35,6 +35,28 @@ Public Function WritePrivateProfileString(
             ByVal lpFileName As String) As Integer
 End Function
 
+Public Const SRCAND As Integer = &H8800C6
+Public Const SRCCOPY As Integer = &HCC0020
+Public Const SRCPAINT As Integer = &HEE0086
+
+<DllImport("user32.dll")> _
+Public Shared Function GetDC(ByVal hWnd As IntPtr) As IntPtr
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Shared Function BitBlt(ByVal hDestDC As IntPtr, _
+    ByVal X As Integer, ByVal Y As Integer, _
+    ByVal nWidth As Integer, ByVal nHeight As Integer, _
+    ByVal hSrcDC As IntPtr, _
+    ByVal xSrc As Integer, ByVal ySrc As Integer, _
+    ByVal dwRop As Integer) As Integer
+End Function
+
+<DllImport("user32.dll")> _
+Public Shared Function ReleaseDC(ByVal hWnd As IntPtr, _
+    ByVal hDC As IntPtr) As IntPtr
+End Function
+
 ''========================================================================
 Public Function getAppPath() As String
 ''--------------------------------------------------------------------
