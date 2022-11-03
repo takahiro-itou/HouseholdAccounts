3d55ebda3bf6b9ba2d73456d708475b9a348a7e2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-03 18:33:10 +0900

    API 宣言を追加

--- View/View/WinAPI.vb
+++ View/View/WinAPI.vb
@@ -39,10 +39,6 @@ Public Const SRCAND As Integer = &H8800C6
 Public Const SRCCOPY As Integer = &HCC0020
 Public Const SRCPAINT As Integer = &HEE0086
 
-<DllImport("user32.dll")> _
-Public Function GetDC(ByVal hWnd As IntPtr) As IntPtr
-End Function
-
 <DllImport("gdi32.dll")> _
 Public Function BitBlt(ByVal hDestDC As IntPtr, _
     ByVal X As Integer, ByVal Y As Integer, _
@@ -52,9 +48,27 @@ Public Function BitBlt(ByVal hDestDC As IntPtr, _
     ByVal dwRop As Integer) As Integer
 End Function
 
+<DllImport("gdi32.dll")> _
+Public Function CreateCompatibleDC(ByVal hDC As IntPtr) As IntPtr
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Function DeleteDC(ByVal hDC As IntPtr) As Integer
+End Function
+
+<DllImport("user32.dll")> _
+Public Function GetDC(ByVal hWnd As IntPtr) As IntPtr
+End Function
+
+
 <DllImport("user32.dll")> _
 Public Function ReleaseDC(ByVal hWnd As IntPtr, _
     ByVal hDC As IntPtr) As IntPtr
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Function SelectObject( _
+    ByVal hDC As IntPtr, ByVal hGdiObj As IntPtr) As IntPtr
 End Function
 
 ''========================================================================
