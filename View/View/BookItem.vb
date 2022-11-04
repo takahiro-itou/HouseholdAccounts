80a6b4475678177f68f47d14adacde65fe532d54
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-04 23:14:18 +0900

    関数をコピー

--- View/View/BookItem.vb
+++ View/View/BookItem.vb
@@ -12,6 +12,33 @@ Module BookItem
 ' This file is written in 2006/09/23 - 2008/01/06
 '*****************************************************************************
 
+Public Function BookItemGetItemType(ByRef utBookItems As tBookItems, _
+    ByVal lngItemIndex As Integer) As Integer
+'---------------------------------------------------------------------
+'項目の種類を返す
+'[ IN] utBookItems : 項目一覧データ
+'[ IN] lngItemIndex: 項目番号
+'[RET] Long
+'---------------------------------------------------------------------
+Dim lngType As Integer
+Dim lngParentHandle As Integer
+
+    With utBookItems
+        Do While (lngItemIndex >= 0)
+            lngType = (.nFlags(lngItemIndex) And ITEM_FLAG_TYPEMASK)
+            lngParentHandle = .utItemEntries(lngItemIndex).nParentHandle
+
+            If (lngType = ITEM_FLAG_INHERIT) Then
+                lngItemIndex = lngParentHandle
+            Else
+                Exit Do
+            End If
+        Loop
+    End With
+
+    BookItemGetItemType = lngType
+End Function
+
 Public Function BookItemGetRegisteredItemCount(ByRef utBookItems As tBookItems) As Long
 '---------------------------------------------------------------------
 '使用済みの項目数を返す
