f57c81a7c17a6ec043591995525d1b5e4db2eb02
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-06 21:40:43 +0900

    関数をコピー

--- View/View/AnnualRecord.vb
+++ View/View/AnnualRecord.vb
@@ -96,4 +96,102 @@ Dim lngTotal As Integer
     AnnualRecordGetItemYearTotal = lngTotal
 End Function
 
+Public Function ReadAnnualRecords(ByRef utRecord As tAnnualRecords,
+        ByVal lngFileNumber As Integer,
+        ByVal lngItemBufferSize As Integer) As Integer
+'---------------------------------------------------------------------
+'ファイルから、一年分のレコードを読み込む
+'[I/O] utRecord          : 年間レコード
+'[ IN] lngFileNumber     : ファイル番号
+'[ IN] lngItemBufferSize : 項目バッファのサイズ
+'[RET] Long
+'  読み込んだバイト数
+'[ACT]
+'  ファイルは予め開いて先頭位置にシークした状態でファイル番号を渡す
+'---------------------------------------------------------------------
+Dim lngStartPos As Integer, lngEndPos As Integer
+
+    lngStartPos = Seek(lngFileNumber) - 1
+
+    lngEndPos = Seek(lngFileNumber) - 1
+    ReadAnnualRecords = (lngEndPos - lngStartPos)
+End Function
+
+Public Function ReallocAnnualRecordsBuffers(ByRef utRecord As tAnnualRecords,
+        ByVal lngItemBufferSize As Integer,
+        ByVal lngStartYear As Integer, ByVal lngNumYears As Integer) As Boolean
+'---------------------------------------------------------------------
+'年間レコード用のバッファを確保しなおす
+'[I/O] utRecord          : 年間レコード
+'[ IN] lngItemBufferSize : 項目バッファのサイズ
+'[ IN] lngStartYear      : 開始西暦年
+'[ IN] lngNumYears       : 年数
+'[RET] Boolean
+'  成功したらTrue, 失敗したら False
+'---------------------------------------------------------------------
+Dim i As Integer
+
+    With utRecord
+        If (lngItemBufferSize < 0) Then lngItemBufferSize = .nItemBufferSize
+
+        'If (lngItemBufferSize < 0) Then lngItemBufferSize = .nItemBufferSize
+
+        ReDim Preserve .utItemAnnualCounts(0 To lngItemBufferSize - 1)
+        ReDim Preserve .utItemDetailCounts(0 To lngItemBufferSize - 1)
+        .nItemBufferSize = lngItemBufferSize
+
+        If (lngNumYears > 0) Then
+            For i = 0 To lngItemBufferSize - 1
+                With .utItemAnnualCounts(i)
+                    ReDim Preserve .nStartValues(0 To lngNumYears - 1)
+                    ReDim Preserve .nEndValues(0 To lngNumYears - 1)
+                    ReDim Preserve .nYearTotal(0 To lngNumYears - 1)
+                End With
+            Next i
+        End If
+    End With
+
+    ReallocAnnualRecordsBuffers = True
+End Function
+
+Public Function RecountAnnualRecords(ByRef utRecord As tAnnualRecords,
+        ByRef lngItemFlags() As Integer, ByVal lngItemBufferSize As Integer,
+        ByVal lngYear As Integer, ByVal lngNumDays As Integer) As Boolean
+'---------------------------------------------------------------------
+'年間レコードを再集計する
+'[I/O] utRecord          : 年間レコード
+'[ IN] lngItemFlags()    : 各項目のフラグ
+'[ IN] lngItemBufferSize : 項目バッファのサイズ
+'[RET] Boolean
+'  成功したらTrue, 何かエラーがあれば False
+'---------------------------------------------------------------------
+Dim i As Integer
+Dim lngDate As Integer, lngStartDayIndex As Integer, lngEndDayIndex As Integer
+
+    '初期値を書き込む
+    With utRecord
+        For i = 0 To lngItemBufferSize - 1
+            If ((lngItemFlags(i) And ITEM_FLAG_TYPEMASK) = ITEM_FLAG_BALANCE) Then
+                .utItemDetailCounts(i).nDayTotal(0) = .utItemAnnualCounts(i).nStartValues(lngYear)
+            End If
+        Next i
+
+        lngStartDayIndex = GetWeekday(lngYear, 1, 1)
+        lngEndDayIndex = GetDayInYear(lngYear, 12, 31) + lngStartDayIndex
+
+        '全てのレシートを集計する
+        For lngDate = 0 To lngNumDays - 1
+            'バッファをクリアする
+            For i = 0 To lngItemBufferSize - 1
+                If ((lngItemFlags(i) And ITEM_FLAG_TYPEMASK) = ITEM_FLAG_BALANCE) Then
+                Else
+                    .utItemDetailCounts(i).nDayTotal(lngDate) = 0
+                End If
+            Next i
+        Next lngDate
+    End With
+
+    RecountAnnualRecords = True
+End Function
+
 End Module
