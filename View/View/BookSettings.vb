2d11a9b5b907e83b81ab3bb5c64c2ac4f4d989af
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-06 22:46:10 +0900

    関数をコピー

--- View/View/BookSettings.vb
+++ View/View/BookSettings.vb
@@ -52,4 +52,87 @@ Dim lngResult As Integer
     AllocBookItems = lngResult
 End Function
 
+Public Function BookItemAllocNewItem(ByRef utBook As tAccountBook) As Integer
+'---------------------------------------------------------------------
+'新しい項目用の領域を確保し、そのハンドルを返す
+'[I/O] utBook: 家計簿データ
+'[RET] Long
+'  新しい項目用のハンドル
+'---------------------------------------------------------------------
+Dim i As Integer
+Dim lngBufferSize As Integer
+Dim lngResult As Integer
+
+    With utBook
+        'バッファサイズと登録済み項目数を比較し、
+        'バッファに空きがある場合は、
+        '空きを探してそのバンドル(インデックス)を返す
+        lngResult = -1
+        With .utBookItems
+            If (.nRegisteredItemCount < .nItemBufferSize) Then
+                For i = .nRootItemCount To .nItemBufferSize - 1
+                    If (.nFlags(i) = ITEM_FLAG_NOTUSED) Then
+                        lngResult = i
+                        Exit For
+                    End If
+                Next i
+            End If
+        End With
+    End With
+
+    If (lngResult >= 0) Then
+        BookItemAllocNewItem = lngResult
+        Exit Function
+    End If
+
+    'バッファをリサイズし、増えた部分の先頭を確保する
+    BookItemAllocNewItem = AllocBookItems(utBook, lngResult)
+End Function
+
+Public Function InsertNewBookItem(ByRef utBook As tAccountBook,
+        ByVal lngParentItemHandle As Integer, ByVal strName As String,
+        ByVal lngFlags As Integer, ByVal lngStartDate As Integer,
+        ByVal lngStartBalance As Integer) As Integer
+'---------------------------------------------------------------------
+'指定した項目に新しいサブ項目を追加する
+'[I/O] utBookItems         : 項目一覧データ
+'[I/O] utYearRecord        : 年間レコードデータ
+'[ IN] lngParentItemHandle : 親項目のハンドル
+'[ IN] strName             : 項目名
+'[ IN] lngFlags            : 項目フラグ
+'[ IN] lngStartDate        : 開始日
+'[ IN] lngStartBalance     : 開始時金額
+'[RET] Long
+'  追加した項目のハンドル
+'---------------------------------------------------------------------
+Dim lngNewItemHandle As Integer
+
+    '新しい項目用のインデックスを取得する
+    lngNewItemHandle = BookItemAllocNewItem(utBook)
+
+    With utBook.utBookItems
+        'この項目に初期値を書き込む
+        .nFlags(lngNewItemHandle) = lngFlags
+        With .utItemEntries(lngNewItemHandle)
+            .nParentHandle = lngParentItemHandle
+            .sItemName = strName
+            .nSubItemCount = 0
+            .nStartDate = lngStartDate
+            .nStartBalance = lngStartBalance
+        End With
+
+        .nRegisteredItemCount = .nRegisteredItemCount + 1
+
+        '親項目の内容を更新する
+        With .utItemEntries(lngParentItemHandle)
+            ReDim Preserve .nSubItems(0 To .nSubItemCount)
+            .nSubItems(.nSubItemCount) = lngNewItemHandle
+            .nSubItemCount = .nSubItemCount + 1
+        End With
+    End With
+
+    '追加した新しい項目のハンドルを返す
+    InsertNewBookItem = lngNewItemHandle
+End Function
+
 End Module
