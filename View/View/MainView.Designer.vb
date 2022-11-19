a49509baa56f9f146836d117e90158b487c091dc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-19 16:30:15 +0900

    現時点で使っていないコントロールを一時的に除去

--- View/View/MainView.Designer.vb
+++ View/View/MainView.Designer.vb
@@ -226,7 +226,7 @@ Partial Class MainView
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
-        Me.Controls.Add(Me.pnlView)
+        ' Me.Controls.Add(Me.pnlView)
         Me.Controls.Add(Me.picBook)
         Me.Controls.Add(Me.stbMain)
         Me.Controls.Add(Me.tlbMain)
