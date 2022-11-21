0d8c2ad671be7729e237874262f847a7ff27efcd
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-21 21:33:27 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Interface/Receipt.h
@@ -0,0 +1,27 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of Receipt class(es).
+**
+**      @file       Interface/Receipt.h
+**/
+
+#pragma     once
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+
+}   //  End of namespace  Wrapper
