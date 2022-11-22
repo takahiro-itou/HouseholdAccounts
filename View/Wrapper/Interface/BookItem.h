4ee6daae744db689b064dada139415ca379f1d66
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-22 21:40:38 +0900

    列挙型宣言を移植

--- View/Wrapper/Interface/BookItem.h
+++ View/Wrapper/Interface/BookItem.h
@@ -18,6 +18,9 @@
 **      @file       Interface/BookItem.h
 **/
 
+#if !defined( HAWRAPPER_INTERFACE_INCLUDED_BOOK_ITEM_H )
+#    define   HAWRAPPER_INTERFACE_INCLUDED_BOOK_ITEM_H
+
 #pragma     once
 
 #include    "HouseholdAccounts/Common/AccountsTypes.h"
@@ -30,6 +33,29 @@ namespace  Wrapper  {
 //
 //    家計簿内の各項目、口座 (現金を含む) 残高。
 //
+
+public  enum class  ItemFlag
+{
+    ITEM_FLAG_TYPEMASK  = 0x000000ff,
+
+    ITEM_FLAG_NOTUSED       = 0,    /**<  未使用。          **/
+    ITEM_FLAG_INHERIT       = 1,    /**<  親の設定を継承。  **/
+    ITEM_FLAG_BALANCE       = 9,    /**<  残高表示。        **/
+    ITEM_FLAG_INCOME        = 17,   /**<  収入。            **/
+    ITEM_FLAG_OUTLAY        = 18,   /**<  支出。            **/
+    ITEM_FLAG_BANK          = 25,   /**<  銀行口座。        **/
+    ITEM_FLAG_BANK_WITHDRAW = 26,   /**<  口座引出。        **/
+    ITEM_FLAG_BANK_DEPOSIT  = 27,   /**<  口座入金。        **/
+    ITEM_FLAG_BANK_TRANSFER = 28,   /**<  口座振替。        **/
+
+    ITEM_FLAG_EXPANDED      = 0x00000100,
+
+    ITEM_FLAG_NOCOUNT_PARENT    = 0x00010000,
+    ITEM_FLAG_NOCOUNT_ROOT      = 0x00020000,
+    ITEM_FLAG_HIDEN             = 0x10000000,
+    ITEM_FLAG_UNERASABLE        = 0x20000000,
+    ITEM_FLAG_READONLY          = 0x40000000
+};
 
 //========================================================================
 /**
@@ -163,3 +189,5 @@ public:
 };
 
 }   //  End of namespace  Wrapper
+
+#endif
