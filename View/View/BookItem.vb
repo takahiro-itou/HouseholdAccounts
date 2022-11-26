29c37d0fa1128cdbbac0f01010a502b4055dc44c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-26 17:46:12 +0900

    不要になった関数を削除

--- View/View/BookItem.vb
+++ View/View/BookItem.vb
@@ -12,21 +12,6 @@ Module BookItem
 ' This file is written in 2006/09/23 - 2008/01/06
 '*****************************************************************************
 
-Public Function BookItemGetParentItemHandle(
-        ByRef utBookItems As Wrapper.BookItems,
-        ByVal lngItemIndex As Integer) As Integer
-'---------------------------------------------------------------------
-'項目の親項目のハンドルを返す
-'[ IN] utBookItems : 項目一覧データ
-'[ IN] lngItemIndex: 項目番号
-'[RET] Long
-'---------------------------------------------------------------------
-Dim lngParentHandle As Integer
-
-    lngParentHandle = utBookItems.utItemEntries(lngItemIndex).nParentHandle
-    BookItemGetParentItemHandle = lngParentHandle
-End Function
-
 Public Function BookItemRelocateItems(
         ByRef utBookItems As Wrapper.BookItems,
         ByRef lpNewIndex() As Integer) As Integer
