1d51b5a215f1421edc80fe131ee197b2d86ba210
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-27 14:40:02 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Interface/AnnualRecords.h
@@ -0,0 +1,108 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of AnnualRecords class.
+**
+**      @file       Interface/AnnualRecordsh
+**/
+
+#if !defined( HAWRAPPER_INTERFACE_INCLUDED_ANNUAL_RECORDS_H )
+#    define   HAWRAPPER_INTERFACE_INCLUDED_ANNUAL_RECORDS_H
+
+#pragma     once
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+
+//========================================================================
+//
+//    集計されたデータの情報。
+//
+
+//========================================================================
+/**
+**    AnnualRecords  class.
+**/
+
+public value struct AnnualRecords
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+public:
+
+//========================================================================
+//
+//    Properties.
+//
+public:
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+};
+
+}   //  End of namespace  Wrapper
+
+#endif
