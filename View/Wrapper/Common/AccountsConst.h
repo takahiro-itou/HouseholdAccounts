6ffd61370d4c56817100b6b37ce4ad52430ae18c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-27 16:12:00 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Common/AccountsConst.h
@@ -0,0 +1,32 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Constant Definitions.
+**
+**      @file       Common/AccountsConst.h
+**/
+
+#if !defined( HAWRAPPER_COMMON_INCLUDED_ACCOUNTS_CONST_H )
+#    define   HAWRAPPER_COMMON_INCLUDED_ACCOUNTS_CONST_H
+
+#pragma     once
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+
+}   //  End of namespace  Wrapper
+
+#endif
