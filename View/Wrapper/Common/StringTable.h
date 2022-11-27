3de6f828d768e70b3784a20ed10e033a86cde084
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-27 20:26:42 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Common/StringTable.h
@@ -0,0 +1,101 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of StringTable class.
+**
+**      @file       Interface/StringTable.h
+**/
+
+#if !defined( HAWRAPPER_COMMON_INCLUDED_STRING_TABLE_H )
+#    define   HAWRAPPER_INTERFACE_
+
+#pragma     once
+
+#include    "HouseholdAccounts/Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+
+//========================================================================
+//
+//    StringTable  class.
+//
+
+public value struct StringTable
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+};
+
+}   //  End of namespace  Wrapper
+
+#endif
