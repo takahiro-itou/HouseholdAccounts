778139f21e4c562d8e1382392b75d40fc855e98c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-03 11:16:08 +0900

    不要になった関数を削除

--- View/View/AccountBook.vb
+++ View/View/AccountBook.vb
@@ -267,29 +267,6 @@ Dim blnResult As Boolean
     CreateEmptyAccountBook = blnResult
 End Function
 
-Public Function IsDayBeforeStart(
-        ByRef utBook As Wrapper.AccountBook,
-        ByVal lngYear As Integer, ByVal lngDayIndex As Integer) As Boolean
-'---------------------------------------------------------------------
-'指定した日付が、家計簿の開始日より前かどうか調べる
-'[ IN] utBook      : 家計簿データ
-'[ IN] lngYear     : 西暦年
-'[ IN] lngDayIndex : 元日からの日数(オフセット付)
-'[RET] Boolean
-'  開始日より前ならTrue, そうでなければ False
-'---------------------------------------------------------------------
-Dim blnResult As Boolean
-
-    With utBook
-        If (CompareDates(lngYear, lngDayIndex, .nStartYear, .nStartDayIndex) < 0) Then
-            blnResult = True
-        Else
-            blnResult = False
-        End If
-    End With
-    IsDayBeforeStart = blnResult
-End Function
-
 Public Function OpenAccountBook(
         ByRef utBook As Wrapper.AccountBook,
         ByVal strFileName As String) As Boolean
