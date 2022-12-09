c5dc65e74ec190ee4a3de60df76ca55bd4fd3a98
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-09 16:43:14 +0900

    クラスを新規作成

---
+++ View/Wrapper/Common/FilePathUtils.h
@@ -0,0 +1,104 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of TextOperation class.
+**
+**      @file       Common/FilePathUtils.h
+**/
+
+#if !defined( HAWRAPPER_COMMON_INCLUDED_FILE_PATH_UTILS_H )
+#    define   HAWRAPPER_COMMON_INCLUDED_FILE_PATH_UTILS_H
+
+
+#pragma     once
+
+#include    "AccountsTypes.h"
+
+namespace  Wrapper  {
+namespace  Common  {
+
+//========================================================================
+//
+//    FilePathUtils  class.
+//
+
+public ref  class  FilePathUtils
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+};
+
+}   //  End of namespace  Common
+}   //  End of namespace  Wrapper
+
+#endif
