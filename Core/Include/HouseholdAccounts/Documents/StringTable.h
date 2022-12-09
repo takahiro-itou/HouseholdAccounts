6b3f370d3cce466e5cf88f1b61995267762cb82c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-09 10:59:53 +0900

    クラスを新規作成

---
+++ Core/Include/HouseholdAccounts/Documents/StringTable.h
@@ -0,0 +1,120 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of StringTable class.
+**
+**      @file       Documents/StringTable.h
+**/
+
+#if !defined( HACCOUNTS_DOCUMENTS_INCLUDED_STRING_TABLE_H )
+#    define   HACCOUNTS_DOCUMENTS_INCLUDED_STRING_TABLE_H
+
+#include    "HouseholdAccounts/Common/HouseholdAccountsSettings.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Documents  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    StringTable  class.
+//
+/**
+**
+**/
+
+class  StringTable
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+public:
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    StringTable();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~StringTable();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   StringTableTest;
+};
+
+}   //  End of namespace  Documents
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/Documents/StringTable.cpp
@@ -0,0 +1,93 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of StringTable class.
+**
+**      @file       Documents/StringTable.cpp
+**/
+
+#include    "HouseholdAccounts/Documents/StringTable.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Documents  {
+
+namespace  {
+}   //  End of (Unnamed) namespace.
+
+//========================================================================
+//
+//    StringTable  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+StringTable::StringTable()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+StringTable::~StringTable()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Documents
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
