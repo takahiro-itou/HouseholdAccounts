b557f1a7a2e4536734d01d436c316fa691968a2d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-16 19:33:43 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Documents/ItemWiseAggregate.h
@@ -0,0 +1,108 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ItemWiseAggregate class.
+**
+**      @file       Documents/ItemWiseAggregate.h
+**/
+
+#if !defined( HAWRAPPER_DOCUMENTS_INCLUDED_ITEM_WISE_AGGREGATE_H )
+#    define   HAWRAPPER_DOCUMENTS_INCLUDED_ITEM_WISE_AGGREGATE_H
+
+#pragma     once
+
+#include    "Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+//========================================================================
+//
+//    ItemWiseAggregate  class.
+//
+
+/**
+**    項目ごとの集計した結果を管理するクラス。
+**/
+
+public ref  class  ItemWiseAggregate
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+};
+
+}   //  End of namespace  Documents
+}   //  End of namespace  Wrapper
+
+#endif
