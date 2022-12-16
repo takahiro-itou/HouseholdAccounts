875a332df9f5007e9f6cee5aa3883182bf3de87c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-16 19:36:06 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Documents/ItemWiseAggregate.cpp
@@ -0,0 +1,88 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of ItemWiseAggregate class.
+**
+**      @file       Documents/ItemWiseAggregate.cpp
+**/
+
+#include    "PreCompile.h"
+
+#include    "ItemWiseAggregate.h"
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+//========================================================================
+//
+//    ItemWiseAggregate  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+}   //  End of namespace  Wrapper
