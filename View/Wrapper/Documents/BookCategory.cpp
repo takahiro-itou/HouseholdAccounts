9b21bef85a5826599f97a92ca61f91b37d50c682
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-17 15:45:58 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Documents/BookCategory.cpp
@@ -0,0 +1,115 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of BookCategory class.
+**
+**      @file       Documents/BookCategory.cpp
+**/
+
+#include    "PreCompile.h"
+
+#include    "BookCategory.h"
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+//========================================================================
+//
+//    BookCategory  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+BookCategory::BookCategory()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する。
+//  （デストラクタ）。
+//
+
+BookCategory::~BookCategory()
+{
+}
+
+//----------------------------------------------------------------
+//    アンマネージドリソースを破棄する。
+//  （ファイナライザ）。
+//
+
+BookCategory::!BookCategory()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Documents
+}   //  End of namespace  Wrapper
---
+++ View/Wrapper/Documents/BookCategory.h
@@ -0,0 +1,135 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of BookCategory class.
+**
+**      @file       Documents/BookCategory.h
+**/
+
+#if !defined( HAWRAPPER_DOCUMENTS_INCLUDED_BOOK_CATEGORY_H )
+#    define   HAWRAPPER_DOCUMENTS_INCLUDED_BOOK_CATEGORY_H
+
+#pragma     once
+
+#include    "Common/AccountsTypes.h"
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+//========================================================================
+//
+//    BookCategory  class.
+//
+
+/**
+**    家計簿の勘定科目、内訳などの項目を管理するクラス。
+**/
+
+public ref  class  BookCategory
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    BookCategory();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する。
+    **  （デストラクタ）。
+    **
+    **/
+    ~BookCategory();
+
+    //----------------------------------------------------------------
+    /**   アンマネージドリソースを破棄する。
+    **  （ファイナライザ）。
+    **
+    **/
+    !BookCategory();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+
+};
+
+}   //  End of namespace  Documents
+}   //  End of namespace  Wrapper
+
+#endif
