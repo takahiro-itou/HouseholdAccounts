99ef13f95327414a6ce889c32d3025af30fd55d7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-17 13:19:08 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Common/DecimalCurrency.cpp
@@ -0,0 +1,89 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of DecimalCurrency class.
+**
+**      @file       Common/DecimalCurrency.cpp
+**/
+
+#include    "PreCompile.h"
+
+#include    "DecimalCurrency.h"
+
+
+namespace  Wrapper  {
+namespace  Common  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+//========================================================================
+//
+//    CategoryWiseAggregates  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+}   //  End of namespace  Wrapper
---
+++ View/Wrapper/Common/DecimalCurrency.h
@@ -0,0 +1,113 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of DecimalCurrency class.
+**
+**      @file       Common/DecimalCurrency.h
+**/
+
+#if !defined( HAWRAPPER_COMMON_INCLUDED_DECIMAL_CURRENCY_H )
+#    define   HAWRAPPER_COMMON_INCLUDED_DECIMAL_CURRENCY_H
+
+#pragma     once
+
+#include    "AccountsTypes.h"
+
+namespace  Wrapper  {
+namespace  Common  {
+
+//========================================================================
+//
+//    DecimalCurrency  class.
+//
+
+/**
+**    固定小数点型の通貨クラス。
+**/
+
+public ref  class  DecimalCurrency
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+
+};
+
+}   //  End of namespace  Common
+}   //  End of namespace  Wrapper
+
+#endif
