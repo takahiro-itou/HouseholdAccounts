b1be1846e4e32f47a81de84a2a74c98c7d444d76
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-20 21:14:47 +0900

    ファイルを新規作成

---
+++ Core/Include/HouseholdAccounts/Common/DecimalCurrency.h
@@ -0,0 +1,119 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of DecimalCurrency class.
+**
+**      @file       Common/DecimalCurrency.h
+**/
+
+#if !defined( HACORE_COMMON_INCLUDED_DECIMAL_CURRENCY_H )
+#    define   HACORE_COMMON_INCLUDED_DECIMAL_CURRENCY_H
+
+#include    "AccountsTypes.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    DecimalCurrency  class.
+//
+/**
+**
+**/
+
+class  DecimalCurrency
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    DecimalCurrency();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~DecimalCurrency();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   DecimalCurrencyTest;
+};
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/Common/DecimalCurrency.cpp
@@ -0,0 +1,93 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of DecimalCurrency class.
+**
+**      @file       Common/DecimalCurrency.cpp
+**/
+
+#include    "HouseholdAccounts/Common/DecimalCurrency.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+namespace  {
+}   //  End of (Unnamed) namespace.
+
+//========================================================================
+//
+//    DecimalCurrency  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+DecimalCurrency::DecimalCurrency()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+DecimalCurrency::~DecimalCurrency()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
