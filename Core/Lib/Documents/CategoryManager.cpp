f0b88d61a555a8d47426fe5849da7d418005f021
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-20 20:58:11 +0900

    ファイルを新規作成

---
+++ Core/Lib/Documents/CategoryManager.cpp
@@ -0,0 +1,93 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of CategoryManager class.
+**
+**      @file       Documents/CategoryManager.cpp
+**/
+
+#include    "HouseholdAccounts/Documents/CategoryManager.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Documents  {
+
+namespace  {
+}   //  End of (Unnamed) namespace.
+
+//========================================================================
+//
+//    CategoryManager  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+CategoryManager::CategoryManager()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+CategoryManager::~CategoryManager()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Documents
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
