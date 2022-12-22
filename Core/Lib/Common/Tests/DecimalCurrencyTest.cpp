0f50fc044d91976d7660e61bfb34c7b93de1610a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-22 20:42:55 +0900

    単体テストクラスを新規作成

---
+++ Core/Lib/Common/Tests/DecimalCurrencyTest.cpp
@@ -0,0 +1,75 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'DecimalCurrency'.
+**
+**      @file       Common/Tests/DecimalCurrencyTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "HouseholdAccounts/Common/DecimalCurrency.h"
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    DecimalCurrencyTest  class.
+//
+/**
+**    クラス DecimalCurrency  の単体テスト。
+**/
+
+class  DecimalCurrencyTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(DecimalCurrencyTest);
+    CPPUNIT_TEST(testDecimalCurrency);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testDecimalCurrency();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( DecimalCurrencyTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  DecimalCurrencyTest::testDecimalCurrency()
+{
+    DecimalCurrency     testee;
+    return;
+}
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
