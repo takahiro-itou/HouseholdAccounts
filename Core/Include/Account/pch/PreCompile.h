368bf7b7a2ebe2cf7909b5f5eb4d228cae59316c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-24 17:55:46 +0900

    ヘッダファイルを新規作成

---
+++ Core/Include/Account/pch/PreCompile.h
@@ -0,0 +1,25 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Pre-Compiled Header File.
+**
+**      @file       pch/PreCompile.h
+**/
+
+#if !defined( HACORE_PCH_INCLUDED_PRE_COMPILE_H )
+#    define   HACORE_PCH_INCLUDED_PRE_COMPILE_H
+
+
+#endif
---
+++ Core/Lib/pch/PreCompile.cpp
@@ -0,0 +1,21 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Generate Pre-Compiled Header File.
+**
+**      @file       pch/PreCompile.h
+**/
+
+#include    "Account/pch/PreCompile.h"
