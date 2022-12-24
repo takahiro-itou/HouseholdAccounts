a05bcd3e6095575b2b0ef6c065e006f78394e8a0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-12-24 11:54:17 +0900

    列挙型の宣言を追加

--- Core/Include/HouseholdAccounts/Documents/BookCategory.h
+++ Core/Include/HouseholdAccounts/Documents/BookCategory.h
@@ -30,10 +30,51 @@ namespace  Documents  {
 
 //========================================================================
 //
+//    Type Definitions.
+//
+
+/**
+**    項目用フラグ。
+**/
+
+enum class  CategoryFlags
+{
+    CTYPE_MASK              = 0x000000ff,
+    CTYPE_NOTUSED           = 0,    /**<  未使用。          **/
+    CTYE_INHERIT            = 1,    /**<  親の設定を継承。  **/
+    CTYPE_BALANCE           = 9,    /**<  残高表示。        **/
+    CTYPE_INCOME            = 17,   /**<  収入。            **/
+    CTYPE_OUTLAY            = 18,   /**<  支出。            **/
+    CTYPE_BANK              = 25,   /**<  銀行口座。        **/
+    CTYPE_BANK_WITHDRAW     = 26,   /**<  口座引出。        **/
+    CTYPE_BANK_DEPOSIT      = 27,   /**<  口座入金。        **/
+    CTYPE_BANK_TRANSFER     = 28,   /**<  口座振替。        **/
+
+    /**   サブ項目を展開表示する。          **/
+    CFLAG_EXPANDED          = 0x00000100,
+
+    /**   親の項目の合計に加算しない。      **/
+    CFLAG_NOCOUNT_PARENT    = 0x00010000,
+
+    /**   ルート項目の合計に加算しない。    **/
+    CFLAG_NOCOUNT_ROOT      = 0x00020000,
+
+    /**   表示しない隠し属性。  **/
+    CFLAG_HIDDEN            = 0x10000000,
+
+    /**   削除不能な予約項目。  **/
+    CFLAG_UNERASABLE        = 0x20000000,
+
+    /**   読み取り専用属性。    **/
+    CFLAG_READ_ONLY         = 0x40000000,
+};
+
+//========================================================================
+//
 //    BookCategory  class.
 //
 /**
-**
+**    家計簿の勘定科目、内訳などの項目を管理するクラス。
 **/
 
 class  BookCategory
