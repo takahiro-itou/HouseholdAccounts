4f56bfccdf3de3c4eec114388fd7d0b80be363ec
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-01 23:43:10 +0900

    クラスを新規作成

---
+++ Core/Include/Account/Common/StrictVector.h
@@ -0,0 +1,123 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of StrictVector template class.
+**
+**      @file       Common/StrictVector.h
+**/
+
+#if !defined( HACORE_COMMON_INCLUDED_STRICT_VECTOR_H )
+#    define   HACORE_COMMON_INCLUDED_STRICT_VECTOR_H
+
+
+#if !defined( HACORE_COMMON_INCLUDED_ACCOUNTS_PROJECT_H )
+#    include    "Account/Common/AccountsProject.h"
+#endif
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    StrictVector  class.
+//
+/**
+**
+**/
+
+template <typename TEl, typename TId>
+class  StrictVector
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+private:
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Operators).
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   StrictVectorTest;
+};
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+//  Implementation.
+#include    "StrictVector.inl"
+
+#endif
