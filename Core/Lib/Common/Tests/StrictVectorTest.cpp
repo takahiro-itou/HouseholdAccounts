756dc6bd3fd83221aa44236f2b8c84e33008a3f0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-01 23:55:34 +0900

    ファイルを新規作成

---
+++ Core/Lib/Common/Tests/StrictVectorTest.cpp
@@ -0,0 +1,29 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'StrictVector'.
+**
+**      @file       Common/Tests/StrictVectorTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "Account/Common/StrictVector.h"
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
