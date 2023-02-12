b05ecdb9b90701cda4dcbd709e5eb0bc22b80368
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-02-12 19:10:43 +0900

    コンパイルエラーを修正

--- Core/Lib/Documents/Tests/StringTableTest.cpp
+++ Core/Lib/Documents/Tests/StringTableTest.cpp
@@ -154,7 +154,7 @@ void  StringTableTest::testSearchEntry()
     CPPUNIT_ASSERT_EQUAL( 3, TO_VALUE_FROM_STRICT(result.siInsert) );
 
     result  = testee.searchEntryTest("AE");
-    CPPUNIT_ASSERT_EQUAL( false, TO_BOOL_FROM_STRICT(result.flgFound) );
+    CPPUNIT_ASSERT( !TO_BOOL_FROM_STRICT(result.flgFound) );
     CPPUNIT_ASSERT_EQUAL(-1, TO_VALUE_FROM_STRICT(result.siResult) );
     CPPUNIT_ASSERT_EQUAL( 4, TO_VALUE_FROM_STRICT(result.siInsert) );
 
