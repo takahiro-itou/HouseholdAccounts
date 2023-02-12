52f8a61ceab6f812869cb7b7d8fced2e9519b66c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-02-12 22:20:24 +0900

    ファイルを新規作成

---
+++ View/Wrapper/Documents/StringTable.cpp
@@ -0,0 +1,92 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of StringTable class.
+**
+**      @file       Documents/StringTable.cpp
+**/
+
+#include    "PreCompile.h"
+
+#include    "StringTable.h"
+
+#include    <msclr/marshal_cppstd.h>
+using       namespace   msclr::interop;
+
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+//========================================================================
+//
+//    StringTable  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Documents
+}   //  End of namespace  Wrapper
---
+++ View/Wrapper/Documents/StringTable.h
@@ -0,0 +1,122 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---  Household Accounts  Wrapper Lib.  ---              **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of StringTable class.
+**
+**      @file       Documents/StringTable.h
+**/
+
+#if !defined( HAWRAPPER_DOCUMENTS_INCLUDED_STRING_TABLE_H )
+#    define   HAWRAPPER_DOCUMENTS_INCLUDED_STRING_TABLE_H
+
+#pragma     once
+
+
+#if !defined( HAWRAPPER_COMMON_INCLUDED_ACCOUNTS_TYPES_H )
+#    include    "Wrapper/Common/AccountsTypes.h"
+#endif
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+
+//  クラスの前方宣言。  //
+namespace  Documents  {
+class   StringTable;
+}   //  End of namespace  Documents
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+
+namespace  Wrapper  {
+namespace  Documents  {
+
+//========================================================================
+//
+//    StringTable  class.
+//
+
+public ref  class  StringTable
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+
+};
+
+}   //  End of namespace  Documents
+}   //  End of namespace  Wrapper
+
+#endif
