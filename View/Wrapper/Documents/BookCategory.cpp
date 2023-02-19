6e24d4115b61abaac3fe201f3c80a34998963d22
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-02-19 09:35:48 +0900

    リファクタリング

--- View/Wrapper/Documents/BookCategory.cpp
+++ View/Wrapper/Documents/BookCategory.cpp
@@ -131,10 +131,10 @@ System::Boolean
 BookCategory::expandCategory(
         const  System::Boolean  blnExpanded)
 {
-    return ( fromNativeBoolean(
-                     this->m_ptrObj->expandCategory(
-                             toNativeBoolean(blnExpanded)
-    ) ) );
+    return  fromNativeBoolean(
+                this->m_ptrObj->expandCategory(
+                        toNativeBoolean(blnExpanded)
+    ) );
 }
 
 //========================================================================
@@ -149,7 +149,7 @@ BookCategory::expandCategory(
 CategoryFlags
 BookCategory::getCategoryType()
 {
-    return ( static_cast<CategoryFlags>(this->m_ptrObj->getCategoryType()) );
+    return  static_cast<CategoryFlags>(this->m_ptrObj->getCategoryType());
 }
 
 //----------------------------------------------------------------
@@ -159,7 +159,7 @@ BookCategory::getCategoryType()
 CategoryFlags
 BookCategory::getFlags()
 {
-    return ( static_cast<CategoryFlags>(this->m_ptrObj->getFlags()) );
+    return  static_cast<CategoryFlags>(this->m_ptrObj->getFlags());
 }
 
 //----------------------------------------------------------------
@@ -169,7 +169,7 @@ BookCategory::getFlags()
 BookCategory::CategoryHandleArray ^
 BookCategory::getSubCategories()
 {
-    return ( toManageFromVector(this->m_ptrObj->getSubCategories()) );
+    return  toManageFromVector(this->m_ptrObj->getSubCategories());
 }
 
 //----------------------------------------------------------------
@@ -179,7 +179,7 @@ BookCategory::getSubCategories()
 System::Boolean
 BookCategory::isExpanded()
 {
-    return ( fromNativeBoolean(this->m_ptrObj->isExpanded()) );
+    return  fromNativeBoolean(this->m_ptrObj->isExpanded());
 }
 
 //========================================================================
@@ -194,7 +194,7 @@ BookCategory::isExpanded()
 CategoryFlags
 BookCategory::Flags::get()
 {
-    return ( static_cast<CategoryFlags>(this->m_ptrObj->getFlags()) );
+    return  static_cast<CategoryFlags>(this->m_ptrObj->getFlags());
 }
 
 //----------------------------------------------------------------
@@ -204,7 +204,7 @@ BookCategory::Flags::get()
 CategoryHandle
 BookCategory::ParentHandle::get()
 {
-    return ( this->m_ptrObj->getParentHandle() );
+    return  this->m_ptrObj->getParentHandle();
 }
 
 //----------------------------------------------------------------
@@ -214,7 +214,7 @@ BookCategory::ParentHandle::get()
 System::String^
 BookCategory::CategoryName::get()
 {
-    return ( marshal_as<System::String^>(this->m_ptrObj->getCategoryName()) );
+    return  marshal_as<System::String^>(this->m_ptrObj->getCategoryName());
 }
 
 //----------------------------------------------------------------
@@ -224,7 +224,7 @@ BookCategory::CategoryName::get()
 CategoryHandle
 BookCategory::NumSubCategories::get()
 {
-    return ( this->m_ptrObj->getNumSubCategories() );
+    return  this->m_ptrObj->getNumSubCategories();
 }
 
 //----------------------------------------------------------------
@@ -234,7 +234,7 @@ BookCategory::NumSubCategories::get()
 Common::DecimalCurrency
 BookCategory::StartBalance::get()
 {
-    return ( DecimalCurrency(this->m_ptrObj->getStartBalance()) );
+    return  DecimalCurrency(this->m_ptrObj->getStartBalance());
 }
 
 //----------------------------------------------------------------
@@ -244,7 +244,7 @@ BookCategory::StartBalance::get()
 DateSerial
 BookCategory::StartDate::get()
 {
-    return ( this->m_ptrObj->getStartDate() );
+    return  this->m_ptrObj->getStartDate();
 }
 
 //----------------------------------------------------------------
@@ -255,7 +255,7 @@ CategoryHandle
 BookCategory::SubCategory::get(
         CategoryHandle  idxSub)
 {
-    return ( this->m_ptrObj->getSubCategory(idxSub) );
+    return  this->m_ptrObj->getSubCategory(idxSub);
 }
 
 //========================================================================
