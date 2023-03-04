e8e5bcaf1cfff5019a5e31065a5ece7817b29196
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-04 19:48:03 +0900

    単体テストクラスを新規作成

---
+++ Core/Lib/DocCls/Tests/AggregatesBlockTest.cpp
@@ -0,0 +1,77 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'AggregatesBlock'.
+**
+**      @file       DocCls/Tests/AggregatesBlockTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "Account/DocCls/AggregatesBlock.h"
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  DocCls  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    AggregatesBlockTest  class.
+//
+/**
+**    クラス AggregatesBlock  の単体テスト。
+**/
+
+class  AggregatesBlockTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(AggregatesBlockTest);
+    CPPUNIT_TEST(testAggregatesBlock);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testAggregatesBlock();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( AggregatesBlockTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  AggregatesBlockTest::testAggregatesBlock()
+{
+    AggregatesBlock     testee;
+
+    return;
+}
+
+}   //  End of namespace  DocCls
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
