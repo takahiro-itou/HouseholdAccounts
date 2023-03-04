92a89ac3505796d11c390fc714ead162c1e226ac
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-04 18:54:59 +0900

    構造体 AggregatesBlock  を新規作成

---
+++ Core/Include/Account/DocCls/AggregatesBlock.h
@@ -0,0 +1,138 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of AggregatesBlock class.
+**
+**      @file       DocCls/AggregatesBlock.h
+**/
+
+#if !defined( HACORE_DOCCLS_INCLUDED_AGGREGATES_BLOCK_H )
+#    define   HACORE_DOCCLS_INCLUDED_AGGREGATES_BLOCK_H
+
+
+#if !defined( HACORE_DOCCLS_INCLUDED_MERCHANDISE_H )
+#    include    "Merchandise.h"
+#endif
+
+#if !defined( HACORE_COMMON_INCLUDED_STRICT_VECTOR_H )
+#    include    "Account/Common/StrictVector.h"
+#endif
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  DocCls  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    AggregatesBlock  struct.
+//
+/**
+**    集計ブロック。
+**/
+
+struct  AggregatesBlock
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    AggregatesBlock();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    ~AggregatesBlock();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+public:
+
+//========================================================================
+//
+//    Public Member Functions (Operators).
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+public:
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   AggregatesBlockTest;
+};
+
+}   //  End of namespace  DocCls
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/DocCls/AggregatesBlock.cpp
@@ -0,0 +1,105 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of AggregatesBlock class.
+**
+**      @file       DocCls/AggregatesBlock.cpp
+**/
+
+#include    "Account/pch/PreCompile.h"
+#include    "Account/DocCls/AggregatesBlock.h"
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  DocCls  {
+
+namespace  {
+}   //  End of (Unnamed) namespace.
+
+//========================================================================
+//
+//    AggregatesBlock  struct.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+AggregatesBlock::AggregatesBlock()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+AggregatesBlock::~AggregatesBlock()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Operators).
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  DocCls
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
