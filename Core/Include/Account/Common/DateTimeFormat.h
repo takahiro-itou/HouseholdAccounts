9208171be1e447619330386a5861c20d7badcbfe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-05 21:36:45 +0900

    クラス DateTimeFormat を新規作成

---
+++ Core/Include/Account/Common/DateTimeFormat.h
@@ -0,0 +1,120 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of DateTimeFormat class.
+**
+**      @file       Common/DateTimeFormat.h
+**/
+
+#if !defined( HACORE_COMMON_INCLUDED_DATE_TIME_FORMAT_H )
+#    define   HACORE_COMMON_INCLUDED_DATE_TIME_FORMAT_H
+
+
+#if !defined( HACORE_COMMON_INCLUDED_ACCOUTNS_TYPES_H )
+#    include    "AccountsTypes.h"
+#endif
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Common  {
+
+
+//========================================================================
+//
+//    DateTimeFormat  class.
+//
+/**
+**
+**/
+
+class  DateTimeFormat
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Operators).
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     DateTimeFormat      This;
+    DateTimeFormat      (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   DateTimeFormatTest;
+};
+
+}   //  End of namespace  Common
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+#endif
