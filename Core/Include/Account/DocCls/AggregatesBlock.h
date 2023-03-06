c2ec74222bbcf3cf672e3774ce619a3d11d2d0d9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-06 19:20:43 +0900

    型宣言のアクセス修飾子を変更

--- Core/Include/Account/DocCls/AggregatesBlock.h
+++ Core/Include/Account/DocCls/AggregatesBlock.h
@@ -63,6 +63,8 @@ public:
         BANK_TRANSFER,
     };
 
+private:
+
     typedef     std::vector<Merchandise>    MerchandiseList;
 
 //========================================================================
