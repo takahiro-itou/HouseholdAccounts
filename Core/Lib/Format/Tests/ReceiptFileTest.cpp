adc18d1b1216ac81a47d074624250817eb8f19cb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-21 18:19:23 +0900

    単体テストクラスを新規作成

---
+++ Core/Lib/Format/Tests/ReceiptFileTest.cpp
@@ -0,0 +1,77 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Household Accounts Core.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2023, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'ReceiptFile'.
+**
+**      @file       Format/Tests/ReceiptFileTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "Account/Format/ReceiptFile.h"
+
+
+HOUSEHOLD_ACCOUNTS_NAMESPACE_BEGIN
+namespace  Format  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    ReceiptFileTest  class.
+//
+/**
+**    クラス ReceiptFile  の単体テスト。
+**/
+
+class  ReceiptFileTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(ReceiptFileTest);
+    CPPUNIT_TEST(testReceiptFile);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testReceiptFile();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( ReceiptFileTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  ReceiptFileTest::testReceiptFile()
+{
+    ReceiptFile     testee;
+
+    return;
+}
+
+}   //  End of namespace  Format
+HOUSEHOLD_ACCOUNTS_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
