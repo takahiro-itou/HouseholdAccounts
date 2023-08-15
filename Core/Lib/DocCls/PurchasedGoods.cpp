d0e8bba2a39288ed10e932edb5b9623c4ac96c50
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-08-15 17:22:37 +0900

    警告対応

--- Core/Lib/DocCls/PurchasedGoods.cpp
+++ Core/Lib/DocCls/PurchasedGoods.cpp
@@ -69,9 +69,10 @@ PurchasedGoods::PurchasedGoods()
       unitPrice(0),
       nQuantity(0),
       cDiscount(0),
-      cSubTotal(0),
+      exclusiveTaxVal(0),
       inclusiveTaxVal(0),
-      exclusiveTaxVal(0)
+      cSubTotal(0),
+      pCatMan(nullptr)
 {
 }
 
