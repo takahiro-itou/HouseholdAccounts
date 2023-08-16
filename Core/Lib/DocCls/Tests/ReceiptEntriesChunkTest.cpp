6385535cbcfcda48c663259d690af883b0820b24
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-08-16 22:41:44 +0900

    単体テストケースを実装

--- Core/Lib/DocCls/Tests/ReceiptEntriesChunkTest.cpp
+++ Core/Lib/DocCls/Tests/ReceiptEntriesChunkTest.cpp
@@ -53,6 +53,10 @@ private:
     typedef     CategoryManager     CategoryManager;
 
     Boolean
+    prepareTestData1(
+            ReceiptEntriesChunk *   chunk);
+
+    Boolean
     setupCategoryManager1(
             CategoryManager &cateMan);
 
@@ -68,6 +72,38 @@ CPPUNIT_TEST_SUITE_REGISTRATION( ReceiptEntriesChunkTest );
 //
 //    For Internal Use Only.
 //
+
+Boolean
+ReceiptEntriesChunkTest::prepareTestData1(
+        ReceiptEntriesChunk *   chunk)
+{
+    chunk->blockFlags       = ReceiptEntriesChunk::ChunkInOutFlags::OUTLAY;
+    chunk->chlDebitAccount  = static_cast<CategoryHandle>(2);
+    chunk->chrCreditAccount = static_cast<CategoryHandle>(-1);
+    chunk->cnlDebitAmount   = static_cast<CurrencyNumerator>(0);
+    chunk->cnrCreditAmount  = static_cast<CurrencyNumerator>(0);
+
+    chunk->goodsList.clear();
+    chunk->goodsList.resize(static_cast<PurchaseNumber>(2));
+
+    ReceiptEntriesChunk::PurchasingList  &
+            goods1  = chunk->goodsList;
+
+    {
+        PurchasedGoods &pg0 = goods1[static_cast<PurchaseNumber>(0)];
+        pg0.accountHeadings = static_cast<CategoryHandle>(4);
+        pg0.accountCategory = static_cast<CategoryHandle>(5);
+        pg0.productName     = "Product1";
+        pg0.unitPrice       = static_cast<CurrencyNumerator>(128);
+        pg0.nQuantity       = 2;
+        pg0.cDiscount       = static_cast<CurrencyNumerator>(0);
+        pg0.exclusiveTaxVal = static_cast<CurrencyNumerator>(0);
+        pg0.inclusiveTaxVal = static_cast<CurrencyNumerator>(0);
+        pg0.cSubTotal       = static_cast<CurrencyNumerator>(256);
+    }
+
+    return ( Boolean::BOOL_TRUE );
+}
 
 Boolean
 ReceiptEntriesChunkTest::setupCategoryManager1(
