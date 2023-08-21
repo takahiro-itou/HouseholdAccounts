b6dcc611f8c48b78bfc65eda912d15b51873696b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-08-21 19:38:01 +0900

    メンバ関数を実装

--- Core/Lib/DocCls/ReceiptInfo.cpp
+++ Core/Lib/DocCls/ReceiptInfo.cpp
@@ -133,6 +133,12 @@ ReceiptInfo::writeToStream(
     os  <<  this->m_receiptDate <<  ";"
         <<  this->m_receiptTime <<  ";"
         <<  this->m_shopName;
+
+    const   ChunkIndex  num = this->m_recordChunk.size();
+    for ( ChunkIndex i = static_cast<ChunkIndex>(0); i < num; ++ i ) {
+        this->m_recordChunk.at(i).writeToStream(os);
+    }
+
     return ( os );
 }
 
